ee2f92c7ea573c1eed5dca469d64e0baae8456eb
1101137148 <1101137148@gm.kuas.edu.tw>
2015-06-07 09:41:47 +0800

    add CoreTest and WebApp

---
+++ ProductCoreTests/Dao/ProductDaoUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spring.Testing.Microsoft;
+using ProductCore.Dao;
+using ProductCore.Models;
+
+namespace ProductCoreTests.Dao
+{
+    [TestClass]
+    public class ProductDaoUnitTest : AbstractDependencyInjectionSpringContextTests
+    {
+
+        #region Spring 單元測試必寫的內容
+
+        override protected string[] ConfigLocations
+        {
+            get
+            {
+                return new String[] { 
+                    "~/Config/ProductCoreDatabase.xml",
+                    "~/Config/ProductCorePointcut.xml",
+                    "~/Config/ProductCoreTests.xml" 
+                };
+            }
+        }
+
+        #endregion
+
+        public IProductDao ProductDao { get; set; }
+
+        [TestMethod]
+        public void TestProductDao_AddProduct()
+        {
+            Product product = new Product();
+            product.ID = 100;
+            product.Name = "單元測試";
+            product.Price = 200;
+            product.Description = "請做出單元測試";
+            ProductDao.AddProduct(product);
+
+            Product dbProduct = ProductDao.GetProductByName(product.Name);
+            Assert.IsNotNull(dbProduct);
+            Assert.AreEqual(product.Name, dbProduct.Name);
+
+            Console.WriteLine("產品編號為 = " + dbProduct.ID);
+            Console.WriteLine("產品名稱為 = " + dbProduct.Name);
+            Console.WriteLine("產品價格為 = " + dbProduct.Price);
+            Console.WriteLine("產品描述為 = " + dbProduct.Description);
+
+            ProductDao.DeleteProduct(dbProduct);
+            dbProduct = ProductDao.GetProductByName(product.Name);
+            Assert.IsNull(dbProduct);
+        }
+    }
+}
---
+++ ProductCoreTests/Services/Impl/ProductServiceUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spring.Testing.Microsoft;
+using ProductCore.Services;
+using ProductCore.Models;
+
+namespace ProductCoreTests.Services.Impl
+{
+    [TestClass]
+    public class ProductServiceUnitTest : AbstractDependencyInjectionSpringContextTests
+    {
+
+        #region Spring 單元測試必寫的內容
+
+        override protected string[] ConfigLocations
+        {
+            get
+            {
+                return new String[] { 
+                    //assembly://MyAssembly/MyNamespace/ApplicationContext.xml
+                    "~/Config/ProductCoreDatabase.xml",
+                    "~/Config/ProductCorePointcut.xml",
+                    "~/Config/ProductCoreTests.xml" 
+                };
+            }
+        }
+
+        #endregion
+
+        public IProductService ProductService { get; set; }
+
+        [TestMethod]
+        public void TestProductService_AddProduct()
+        {
+            Product product = new Product();
+            product.ID = 300;
+            product.Name = "單元測試";
+            product.Price = 400;
+            product.Description = "請做出單元測試";
+            ProductService.AddProduct(product);
+
+            Product dbProduct = ProductService.GetProductByName(product.Name);
+            Assert.IsNotNull(dbProduct);
+            Assert.AreEqual(product.Name, dbProduct.Name);
+
+            Console.WriteLine("課程編號為 = " + dbProduct.ID);
+            Console.WriteLine("課程名稱為 = " + dbProduct.Name);
+            Console.WriteLine("課程名稱為 = " + dbProduct.Price);
+            Console.WriteLine("課程描述為 = " + dbProduct.Description);
+
+            ProductService.DeleteProduct(dbProduct);
+            dbProduct = ProductService.GetProductByName(product.Name);
+            Assert.IsNull(dbProduct);
+        }
+
+    }
+}
